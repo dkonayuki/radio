e3fab1d724e819dd2f013f2f790c2912ee69bf24
dkonayuki <dkonayuki@gmail.com>
2015-06-23 16:33:29 +0900

    improve ui

--- resources/views/radios/_radio.blade.php
+++ resources/views/radios/_radio.blade.php
@@ -1,12 +1,10 @@
 <div class="radio-item">
-    <div class="col-sm-3 col-md-2">
-        <div class="thumbnail">
-            <img src="{{ asset('images/radio_logo.jpg') }}" alt="...">
-            <div class="caption">  
-                <a href="{{ route('radios.show', $radio->id) }}">
-                    <h4>{{ $radio->name }}</h4>
-                </a>
-            </div>
+    <div class="radio-img">
+        <img src="{{ asset('images/radio_logo.jpg') }}" alt="...">
+        <div class="caption">  
+            <a href="{{ route('radios.show', $radio->id) }}">
+                <h4>{{ $radio->name }}</h4>
+            </a>
         </div>
     </div>
 </div>
