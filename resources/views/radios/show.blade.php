6c366d54739c9b8e2306f60b126542c46da7370c
dkonayuki <dkonayuki@gmail.com>
2015-06-23 14:57:48 +0900

    improve ui

--- resources/views/radios/show.blade.php
+++ resources/views/radios/show.blade.php
@@ -1,6 +1,11 @@
 @extends ('layouts.app')
 
 @section ('content')
+<div id="radio-show">
     <audio controls src="{{ $radio->stream_url }}"></audio>
-<a href="{{ route('radios.edit', $radio->id) }}">Edit</a> | <a href="{{ route('radios.destroy', $radio->id) }}" data-method="delete" class="radio-delete-btn">Delete</a> 
+    <br/>
+    <div id="radio-menu">
+        <a class="btn btn-info" href="{{ route('radios.edit', $radio->id) }}">Edit</a><a class="btn btn-danger" href="{{ route('radios.destroy', $radio->id) }}" data-method="delete" class="radio-delete-btn">Delete</a> 
+    </div>
+</div>
 @endsection
