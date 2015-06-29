1a54fbe38457767947ce09662d6a357c74ba79f2
dkonayuki <dkonayuki@gmail.com>
2015-06-29 13:59:27 +0900

    add more information to radio show page, change login btn to register

--- resources/views/radios/show.blade.php
+++ resources/views/radios/show.blade.php
@@ -2,8 +2,12 @@
 
 @section ('content')
 <div id="radio-show">
+    <h3>{{ $radio->name }}</h3>
     <audio controls src="{{ $radio->stream_url }}"></audio>
     <br/>
+    <div id="radio-des">
+        {{ $radio->description }}
+    </div>
     <div id="radio-menu">
         <a class="btn btn-info" href="{{ route('radios.edit', $radio->id) }}">Edit</a>
         <a id="radio-delete-btn" class="btn btn-danger" href="{{ route('radios.destroy', $radio->id) }}" data-method="delete" class="radio-delete-btn">Delete</a> 
