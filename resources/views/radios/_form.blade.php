e4249964f3b4dadc978e2a2cacdee6f4daa73a81
dkonayuki <dkonayuki@gmail.com>
2015-09-27 16:57:23 +0900

    restructure radios table

--- resources/views/radios/_form.blade.php
+++ resources/views/radios/_form.blade.php
@@ -7,16 +7,12 @@
     {!! Form::text('stream_url') !!}
 </div>
 <div class="form-group">
-    {!! Form::label('schedule_url', 'Schedule URL:') !!}
-    {!! Form::text('schedule_url') !!}
-</div>
-<div class="form-group">
     {!! Form::label('description', 'Description:') !!}
     {!! Form::textarea('description') !!}
 </div>
 <div class="form-group">
-    {!! Form::label('image', 'Image:') !!}
-    {!! Form::file('image') !!}
+    {!! Form::label('logo_url', 'Image:') !!}
+    {!! Form::file('logo_url') !!}
 </div>
 <div class="form-group">
     {!! Form::submit($submit_text, ['class'=>'btn btn-info']) !!}
